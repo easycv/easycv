a8f38522ba5d444f94e881bdf307cc61848e7e88
Diogo Neutel <neuteldiogo@gmail.com>
2020-01-22 23:46:59 +0000

    Finished shif_image.

--- cv/transforms/spatial.py
+++ cv/transforms/spatial.py
@@ -1,4 +1,6 @@
-from cv.errors.transforms import InvalidArgsError
+import numpy as np
+
+from cv.errors.transforms import InvalidArgsError, InvalidMethodError
 
 
 def crop_image(image, box=None):
@@ -6,5 +8,23 @@ def crop_image(image, box=None):
         if box[0][1] == box[1][1] and box[2][1] == box[3][1] and box[0][1] >= 0 and box[3][1] <= image.shape[0]:
             if box[0][0] == box[2][0] and box[1][0] == box[3][0] and box[0][0] >= 0 and box[1][0] <= image.shape[1]:
                 return image[:box[3][1], :box[3][0], :][box[0][1]:, box[0][1]:, :]
-    raise InvalidArgsError(['Box'])
+    raise InvalidArgsError(('Box',))
 
+
+def shift_image(image, direction=(1, 1), fill_mode='fill', fill_value=0):
+    non = lambda s: s if s < 0 else None
+    pop = lambda s: s if s > 0 else None
+    ox, oy = direction
+    image = np.roll(image, oy, axis=0)
+    image = np.roll(image, ox, axis=1)
+    if fill_mode == 'fill':
+        if 0 <= fill_value <= 255:
+            image[non(oy):pop(oy), :, :] = fill_value
+            image[:, non(ox):pop(ox), :] = fill_value
+            return image
+        raise InvalidArgsError(('fill_value',))
+
+    elif fill_mode == 'warp':
+        return image
+    else:
+        raise InvalidMethodError(('Fill', 'Warp'))
