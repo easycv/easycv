affd41c964b56a2c8203ea5b27a1ecbe8ba8cfc5
Ricardo Fonseca <ricardo2000.2@gmail.com>
2020-03-20 17:25:27 +0000

    Noise remake (working)

--- cv/transforms/noise.py
+++ cv/transforms/noise.py
@@ -6,25 +6,32 @@ from skimage.util import random_noise
 class Noise(Transform):
     default_args = {
         'method': Option(['gaussian', 'localvar', 'poisson', 'salt', 'pepper', 's&p', 'speckle'], default=0),
-        'seed': Number(min_value=0, max_value=2**32-1),
+        'seed': Number(min_value=0, max_value=2**32-1, default=False),
         'clip': Type(bool, default=True),
         'mean': Type(float, default=0),
         'var': Type(float, default=0.01),
-
+        'amount': Number(min_value=0, max_value=1, default=0.05),
+        'salt_vs_pepper': Number(min_value=0, max_value=1, default=0.5)
         }
 
     def apply(self, image, **kwargs):
         if kwargs['method'] == 'gaussian':
-            return random_noise(image, mode=kwargs['method'], seed=kwargs['seed'], clip=kwargs['clip'], mean=kwargs['mean'], var=kwargs['var'])
+            return random_noise(image, mode=kwargs['method'], seed=kwargs['seed'] if kwargs['seed'] else None,
+                                clip=kwargs['clip'], mean=kwargs['mean'], var=kwargs['var'])
         elif kwargs['method'] == 'localvar':
-            return random_noise(image, mode=kwargs['method'], seed=kwargs['seed'])
+            return random_noise(image, mode=kwargs['method'], seed=kwargs['seed'] if kwargs['seed'] else None)
         elif kwargs['method'] == 'poisson':
-            return random_noise(image, mode=kwargs['method'], seed=kwargs['seed'], clip=kwargs['clip'])
+            return random_noise(image, mode=kwargs['method'], seed=kwargs['seed'] if kwargs['seed'] else None,
+                                clip=kwargs['clip'])
         elif kwargs['method'] == 'salt':
-            return random_noise(image, mode=kwargs['method'], seed=kwargs['seed'])
+            return random_noise(image, mode=kwargs['method'], seed=kwargs['seed'] if kwargs['seed'] else None,
+                                amount=kwargs['amount'])
         elif kwargs['method'] == 'pepper':
-            return random_noise(image, mode=kwargs['method'], seed=kwargs['seed'])
+            return random_noise(image, mode=kwargs['method'], seed=kwargs['seed'] if kwargs['seed'] else None,
+                                amount=kwargs['amount'])
         elif kwargs['method'] == 's&p':
-            return random_noise(image, mode=kwargs['method'], seed=kwargs['seed'])
+            return random_noise(image, mode=kwargs['method'], seed=kwargs['seed'] if kwargs['seed'] else None,
+                                amount=kwargs['amount'], salt_vs_pepper=kwargs['salt_vs_pepper'])
         elif kwargs['method'] == 'speckle':
-            return random_noise(image, mode=kwargs['method'], seed=kwargs['seed'], clip=kwargs['clip'], mean=kwargs['mean'], var=kwargs['var'])
+            return random_noise(image, mode=kwargs['method'], seed=kwargs['seed'] if kwargs['seed'] else None,
+                                clip=kwargs['clip'], mean=kwargs['mean'], var=kwargs['var'])
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,38 +1,38 @@
 import os
 
 from cv.pipeline import Pipeline
-from cv.transforms.noise import SaltAndPepper, Impulse, Gaussian
+from cv.transforms.noise import Noise
 
 
 def test_name():
-    p = Pipeline([SaltAndPepper(), Impulse(), Gaussian()])
+    p = Pipeline([Noise(method='gaussian'), Noise(method='pepper'), Noise(method='s&p')])
     assert p.name() == 'pipeline'
-    p = Pipeline([SaltAndPepper(), Gaussian()], name='test-pipeline')
+    p = Pipeline([Noise(method='s&p'), Noise(method='gaussian')], name='test-pipeline')
     assert p.name() == 'test-pipeline'
 
 
 def test_description():
-    p = Pipeline([Impulse(), Gaussian(sigma=[])])
-    p2 = Pipeline([Impulse(), Gaussian(sigma=[1, 2])])
+    p = Pipeline([Noise(method='pepper'), Noise(method='gaussian', var=0.2)])
+    p2 = Pipeline([Noise(method='pepper'), Noise(method='gaussian', var=0.2)])
     assert p.description() == p2.description()
-    p3 = Pipeline([Impulse(), Gaussian()])
-    p4 = Pipeline([Impulse(prob=0.124), Gaussian()])
+    p3 = Pipeline([Noise(method='pepper'), Noise(method='gaussian')])
+    p4 = Pipeline([Noise(method='pepper', ammount=0.125), Noise(method='gaussian')])
     assert p3.description() != str(p4)
 
 
 def test_num_transforms():
-    p = Pipeline([SaltAndPepper(), Impulse(), Gaussian()])
-    p2 = Pipeline([SaltAndPepper(), p, Gaussian()])
+    p = Pipeline([Noise(method='s&p'), Noise(method='pepper'), Noise(method='gaussian')])
+    p2 = Pipeline([Noise(method='s&p'), p, Noise(method='gaussian')])
     assert p2.num_transforms() == 5
 
 
 def test_transforms():
-    p = Pipeline([SaltAndPepper(), Impulse(), Gaussian()])
+    p = Pipeline([Noise(method='s&p'), Noise(method='pepper'), Noise(method='gaussian')])
     assert len(p.transforms()) == p.num_transforms()
 
 
 def test_save():
-    p = Pipeline([SaltAndPepper(prob=0.1512), Impulse(), Gaussian(sigma=5)], name='test')
+    p = Pipeline([Noise(method='s&p', amount=0.1523), Noise(method='pepper'), Noise(method='gaussian', var=0.4)], name='test')
     p.save()
     assert os.path.exists("test.pipe")
     p2 = Pipeline("test.pipe")
