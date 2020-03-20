84e3fbc59f26cc77d67ca76a1fc21b92e1165b7a
Ricardo Fonseca <ricardo2000.2@gmail.com>
2020-03-20 03:42:54 +0000

    Test pr check

--- cv/transforms/noise.py
+++ cv/transforms/noise.py
@@ -8,7 +8,7 @@ class SaltAndPepper(Transform):
     default_args = {'prob': 0.05}
 
     def apply(self, image, **kwargs):
-        a = f'test-ci'
+        a = f'test-cighjk'
         return image
 
 
