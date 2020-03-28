4f8417d5953abc209c3effd9324313f83723f9a6
Ricardo Fonseca <ricardo2000.2@gmail.com>
2020-03-28 03:00:16 +0000

    Spacial commented

--- cv/transforms/spatial.py
+++ cv/transforms/spatial.py
@@ -8,14 +8,15 @@ from cv.utils import interpolation_methods
 
 class Resize(Transform):
     """
-    Resize is a transform that resizes a image given the desired widht an height, using a certain
-    interpolation for the missing pixels.
+    Resize is a transform that resizes an image to a given width and height. The interpolation
+    method can be specified by the method parameter.
 
     :param width: Output image width
     :type width: :class:`int`
     :param height: Output image height
-    :type height: :class:`int`, optional
-    :param method: Interpolation method
+    :type height: :class:`int`
+    :param method: Interpolation method, defaults to "cubic" if the image is to be upscaled and to
+    "area"  if downscaled
     :type method: :class:`str`, optional
     """
 
@@ -42,6 +43,19 @@ class Resize(Transform):
 
 
 class Rescale(Transform):
+    """
+        Rescale is a transform that rescales an image to a scale factor for x and y.
+        The interpolation method can be specified by the method parameter.
+
+        :param fx: scale factor along the horizontal axis
+        :type fx: :class:`int`
+        :param fy: scale factor along the vertical axis
+        :type fy: :class:`int`
+        :param method: Interpolation method, defaults to "cubic" if the image is to be upscaled and
+        to "area"  if downscaled
+        :type method: :class:`str`, optional
+    """
+
     default_args = {
         "fx": Number(min_value=0),
         "fy": Number(min_value=0),
@@ -67,6 +81,22 @@ class Rescale(Transform):
 
 
 class Rotate(Transform):
+    """
+            Rotate is a transform that rotates an image by degrees according to the provided
+            center. It can also be scaled. The user can decide whether to adjust the image size to
+            contain the all image,if needed.
+
+            :param degrees: degrees to rotate the image
+            :type degrees: :class:`int`
+            :param scale: scale factor, defaults to 1
+            :type scale: :class:`int`
+            :param center: center by which the image is rotated, defaults to the image center
+            :type center: :class:'list'/'tuple', optional
+            :param bounded: adjusts the image size to contain the all image,if needed.
+            Defaults to True
+            :type bounded: :class:'bool', optional
+        """
+
     default_args = {
         "degrees": Number(),
         "scale": Number(default=1),
@@ -101,14 +131,25 @@ class Rotate(Transform):
 
 
 class Crop(Transform):
+    """
+        Crop is a transform that crops an area given by x and y , if original is True then
+        the image size will be kept and the cropped area blacked
+
+        :param x: x beginning and end, given by a tuple or list (beginning,end)
+        :type x: :class:'list'/'tuple'
+        :param y: y beginning and end, given by a tuple or list (beginning,end)
+        :type y: :class: 'list'/'tuple'
+        :param original: center by which the image is rotated, defaults to the image center
+        :type original: :class:'list'/'tuple', optional
+    """
+
     default_args = {
         "x": List(Number(min_value=0), length=2, default=(0, 0)),
         "y": List(Number(min_value=0), length=2, default=(0, 0)),
-        "box": Type(bool, default=True),
+        "original": Type(bool, default=True),
     }
 
     def apply(self, image, **kwargs):
-        # this is your box top_x
         lx, rx, ty, by = kwargs["x"][0], kwargs["x"][1], kwargs["y"][0], kwargs["y"][1]
 
         #  crops the image keeping the original size
@@ -130,11 +171,20 @@ class Crop(Transform):
 
 
 class Translate(Transform):
+    """
+        Translate is a transform that translates the image according to a vector xy
+
+        :param x: x values of the vector
+        :type x: :class:'int'
+        :param y: y values of the vector
+        :type y: :class: 'int'
+    """
+
     default_args = {"x": Number(default=0), "y": Number(default=0)}
 
     def apply(self, image, **kwargs):
         height, width = image.shape[:2]
 
-        matrix = np.float32([[1, 0, kwargs["xShift"]], [0, 1, kwargs["yShift"]]])
+        matrix = np.float32([[1, 0, kwargs["x"]], [0, 1, kwargs["y"]]])
 
         return cv2.warpAffine(image, matrix, (width, height))
