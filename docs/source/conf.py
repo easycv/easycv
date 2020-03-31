58cfa7485324f7dd437dc3f6402f459a99f507cf
1cybersheep1 <andre.marques.alves@tecnico.ulisboa.pt>
2020-03-31 02:22:33 +0100

    Fixed docs side menu

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,9 @@ exclude_patterns = []
 #
 html_theme_path = ["../_themes"]
 html_theme = "sphinx_rtd_theme"
-
+html_theme_options = {
+    "navigation_depth": 3,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
