d8f3643a57d1fc2222a72ceddd4774862a1b18f5
1cybersheep1 <andre.marques.alves@tecnico.ulisboa.pt>
2020-04-05 19:35:11 +0100

    Changed theme back to lacal

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,6 +53,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+html_theme_path = ["../_themes"]
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     "navigation_depth": 3,
