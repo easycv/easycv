fa44e5919f6e9d222bff6e4f92495179f4263343
1cybersheep1 <andre.marques.alves@tecnico.ulisboa.pt>
2020-04-06 01:30:48 +0100

    Test add imports

---
+++ easycv/transforms/__init__.py
@@ -0,0 +1,21 @@
+__all__ = [
+    "Blur",
+    "Canny",
+    "Crop",
+    "FilterChannels",
+    "Gradient",
+    "GradientAngle",
+    "GradientMagnitude",
+    "GrayScale",
+    "Noise",
+    "Rescale",
+    "Resize",
+    "Rotate",
+    "Translate",
+]
+
+from easycv.transforms.noise import Noise
+from easycv.transforms.filter import Blur
+from easycv.transforms.color import GrayScale, FilterChannels
+from easycv.transforms.spatial import Resize, Rescale, Crop, Rotate, Translate
+from easycv.transforms.edges import Gradient, GradientAngle, GradientMagnitude, Canny
