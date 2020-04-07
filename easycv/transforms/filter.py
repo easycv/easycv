f34f6244a6bf791a54393d07e650936861d22924
Diogo Neutel <neuteldiogo@gmail.com>
2020-04-07 23:25:31 +0100

    Started Sharpen

--- easycv/transforms/filter.py
+++ easycv/transforms/filter.py
@@ -1,7 +1,8 @@
 import cv2
+from skimage.filters import unsharp_mask
 
 from easycv.transforms.base import Transform
-from easycv.validators import Option, Number
+from easycv.validators import Option, Number, Type
 
 
 class Blur(Transform):
@@ -15,7 +16,7 @@ class Blur(Transform):
 
     :param method: Blur method to be used, defaults to "uniform"
     :type method: :class:`str`, optional
-    :param size: Kernel size, defaults to 5
+    :param size: Kernel size, defaults to auto
     :type size: :class:`int`, optional
     :param sigma: Sigma value, defaults to 0
     :type sigma: :class:`int`, optional
@@ -23,20 +24,25 @@ class Blur(Transform):
     :type sigma_color: :class:`int`, optional
     :param sigma_space: Sigma for coordinate space, defaults to 75
     :type sigma_space: :class:`int`, optional
+    :param truncate: Truncate the filter at this many standard deviations., defaults to 4
+    :type truncate: :class:`int`, optional
     """
 
     default_args = {
         "method": Option(["uniform", "gaussian", "median", "bilateral"], default=1),
-        "size": Number(min_value=1, only_integer=True, only_odd=True, default=5),
+        "size": Number(min_value=1, only_integer=True, only_odd=True, default="auto"),
         "sigma": Number(min_value=0, default=0),
         "sigma_color": Number(min_value=0, default=75),
         "sigma_space": Number(min_value=0, default=75),
+        "truncate": Number(min_value=0, default=4),
     }
 
     def apply(self, image, **kwargs):
         if kwargs["method"] == "uniform":
             return cv2.blur(image, (kwargs["size"], kwargs["size"]))
         elif kwargs["method"] == "gaussian":
+            if kwargs["size"] == "auto":
+                kwargs["size"] = int(kwargs["sigma"] * 8 + 1)
             return cv2.GaussianBlur(
                 image, (kwargs["size"], kwargs["size"]), kwargs["sigma"]
             )
@@ -46,3 +52,27 @@ class Blur(Transform):
             return cv2.bilateralFilter(
                 image, kwargs["size"], kwargs["sigma_color"], kwargs["sigma_space"]
             )
+
+
+class Sharpen(Transform):
+    """
+    Sharpen is a transform that sharpens an image.
+    enhanced image = original + amount * (original - blurred)
+
+    :param radius: Blur method to be used, defaults to "uniform"
+    :type radius: :class:`str`, optional
+    :param amount: Kernel size, defaults to auto
+    :type amount: :class:`int`, optional
+    :param multichannel: True
+    :type multichannel: :class:`int`, optional
+    """
+
+    default_args = {
+        "radius": Number(min_value=0, only_integer=True, default=1),
+        "sigma": Number(min_value=0, default=0),
+        "amount": Number(default=1),
+        "multichannel": Type(bool, default=True),
+    }
+
+    def apply(self, image, **kwargs):
+        return unsharp_mask(image, **kwargs, preserve_range=True)
