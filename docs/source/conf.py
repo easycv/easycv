2948b867309232fcd0bbc30e0a51aef6ed4dbb1c
1cybersheep1 <andre.marques.alves@tecnico.ulisboa.pt>
2020-04-08 01:11:53 +0100

    Releasing version 0.1.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, EasyCV Developers"
 author = "EasyCV Developers"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.1"
+release = "0.1.2"
 
 
 # -- General configuration ---------------------------------------------------
