e617f2c8b2bf76cbf74e41489970f85da7b5e98d
1cybersheep1 <andre.marques.alves@tecnico.ulisboa.pt>
2020-06-01 11:51:14 +0100

    Added face and eye detector

--- easycv/transforms/detect.py
+++ easycv/transforms/detect.py
@@ -1,5 +1,10 @@
+import cv2
+
+from easycv.resources import get_resource
 from easycv.transforms.base import Transform
-from easycv.validators import Type, List, Number
+from easycv.transforms.color import GrayScale
+from easycv.transforms.spatial import Crop
+from easycv.validators import Type, List, Number, File
 
 try:
     from pyzbar import pyzbar
@@ -36,3 +41,80 @@ class Scan(Transform):
             rectangles.append([(x, y), (x + width, y + height)])
 
         return {"detections": len(decoded), "data": data, "rectangles": rectangles}
+
+
+class CascadeDetector(Transform):
+
+    arguments = {
+        "cascade": File(),
+        "scale": Number(default=1.1),
+        "min_neighbors": Number(min_value=0, only_integer=True, default=3),
+        "min_size": Number(min_value=0, default="auto"),
+        "max_size": Number(min_value=0, default="auto"),
+    }
+
+    outputs = {
+        "rectangles": List(
+            List(List(Number(min_value=0, only_integer=True), length=2), length=2)
+        ),
+    }
+
+    def process(self, image, **kwargs):
+        cascade = cv2.CascadeClassifier(kwargs["cascade"])
+        gray = GrayScale().apply(image)
+        detections = cascade.detectMultiScale(
+            gray,
+            scaleFactor=kwargs["scale"],
+            minNeighbors=kwargs["min_neighbors"],
+            minSize=kwargs["min_size"] if kwargs["min_size"] != "auto" else None,
+            maxSize=kwargs["max_size"] if kwargs["max_size"] != "auto" else None,
+        )
+
+        rectangles = []
+        for x, y, w, h in detections:
+            rectangles.append([(x, y), (x + w, y + h)])
+
+        return {"rectangles": rectangles}
+
+
+class Faces(Transform):
+
+    outputs = {
+        "rectangles": List(
+            List(List(Number(min_value=0, only_integer=True), length=2), length=2)
+        ),
+    }
+
+    def process(self, image, **kwargs):
+        cascade_file = get_resource(
+            "haar-face-cascade", "haarcascade_frontalface_default.xml"
+        )
+        return CascadeDetector(
+            cascade=str(cascade_file), scale=1.3, min_neighbors=5
+        ).apply(image)
+
+
+class Eyes(Transform):
+
+    outputs = {
+        "rectangles": List(
+            List(List(Number(min_value=0, only_integer=True), length=2), length=2)
+        ),
+    }
+
+    def process(self, image, **kwargs):
+        cascade_file = get_resource("haar-eye-cascade", "haarcascade_eye.xml")
+
+        rectangles = []
+        for face in Faces().apply(image)["rectangles"]:
+            face_image = Crop(rectangle=face).apply(image)
+            eyes = CascadeDetector(cascade=str(cascade_file)).apply(face_image)[
+                "rectangles"
+            ]
+            for eye in eyes:
+                adjusted = []
+                for i in range(len(eye)):
+                    adjusted.append([eye[i][0] + face[0][0], eye[i][1] + face[0][1]])
+                rectangles.append(adjusted)
+
+        return {"rectangles": rectangles}
