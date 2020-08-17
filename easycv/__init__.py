86ab1bdc28c5278fff301ca5059834f53e5d7c18
1cybersheep1 <andre.marques.alves@tecnico.ulisboa.pt>
2020-08-17 18:42:44 +0100

    Added camera main methods

--- easycv/__init__.py
+++ easycv/__init__.py
@@ -4,7 +4,9 @@ __version__ = "0.2.1"
 from easycv.image import Image
 from easycv.pipeline import Pipeline
 from easycv.list import List
+from easycv.camera import Camera
+
 import os
 
 os.environ["SESSION_MANAGER"] = ""
-__all__ = ["Image", "Pipeline", "List"]
+__all__ = ["Image", "Camera", "Pipeline", "List"]
---
+++ easycv/camera.py
@@ -0,0 +1,37 @@
+import cv2
+
+from easycv.pipeline import Pipeline
+from easycv.image import Image
+
+
+class Camera:
+    def __init__(self):
+        self._pending = Pipeline([])
+
+    def show(self, name="Camera"):
+        cap = cv2.VideoCapture(0)
+        ret, frame = cap.read()
+        cv2.namedWindow(name, cv2.WINDOW_KEEPRATIO)
+        cv2.resizeWindow(name, frame.shape[1], frame.shape[0])
+
+        while cv2.getWindowProperty(name, cv2.WND_PROP_VISIBLE) >= 1:
+            cv2.imshow(name, Image(frame).apply(self._pending).array)
+            ret, frame = cap.read()
+            if (cv2.waitKey(1) & 0xFF) == ord("q"):
+                cv2.destroyAllWindows()
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+    def capture(self):
+        cap = cv2.VideoCapture(0)
+        ret, frame = cap.read()
+        cap.release()
+        return Image(frame).apply(self._pending)
+
+    def apply(self, operation):
+        self._pending.add_transform(operation)
+
+    def clear(self):
+        self._pending.clear()
